44479585450373b0a9a6ca5d80b716a63635480d
donatomontrone <donato.montrone@outlook.com>
2023-02-10 14:40:19 +0100

    added inital scaffolding and table migration

---
+++ resources/views/guests/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('main-content')
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                main preview
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
